4cc13480ceb4fe5fc090f6812924fe01b1ddcc0d
Uzair Javaid Rather <uzairjavaidrather111@gmail.com>
2026-05-14 21:17:35 +0530

    updating

--- Day12/codes.py
+++ Day12/codes.py
@@ -1,13 +1,46 @@
 
 """The 'codes.py' module will print a list of any dimension """
 
-def print_list(mylist, level):
+def print_list(mylist, level= 0, indent = True):
     """The function 'print_list(arg1,arg2)' takes 2 arguments as list and level. The purpose is to print the list"""
     for i in mylist:
         if isinstance(i,list):
-            print_list(i,level+1)
+            print_list(i,level+1,indent)
+        else: 
+            if indent: 
+                for j in range(level):
+                    print("\t",end = '')
+        
+            print(i)
             
-            for j in range(level):
-                print("\t",end = '')
-        else:
+            
+            
+            
+#mylist = [0,[1,2],[3,4]]   
+   
+       
+#print_list(mylist)
+
+def print_tuple(mytuple, level= 0, indent = True):
+    """The function 'print_list(arg1,arg2)' takes 2 arguments as tuple and level. The purpose is to print the tuple"""
+    for i in mytuple:
+        if isinstance(i,tuple):
+            print_tuple(i,level+1,indent)
+        else: 
+            if indent: 
+                for j in range(level):
+                    print("\t",end = '')
+        
+            print(i)
+
+def print_set(myset, level= 0, indent = True):
+    """The function 'print_list(arg1,arg2)' takes 2 arguments as set and level. The purpose is to print the set"""
+    for i in myset:
+        if isinstance(i,set):
+            print_list(i,level+1,indent)
+        else: 
+            if indent: 
+                for j in range(level):
+                    print("\t",end = '')
+        
             print(i)
--- Day12/setup.py
+++ Day12/setup.py
@@ -1,10 +1,11 @@
 # Making module sharable
+# Containing the metadata 
 
 from setuptools import setup
 
 setup(
     name = 'codes' ,   #tuple
-    version = '1.0.1' ,
-   # author = 'planB' ,
+    version = '1.3.1' ,
+    author = 'planB' ,
     py_modules = ['codes']
 )
